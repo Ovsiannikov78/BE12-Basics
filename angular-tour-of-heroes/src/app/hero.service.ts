78cf1cc9be1b791008624acd8237a81029afd753
Ovsiannikov78 <seri_tanya@mail.ru>
2021-02-22 16:30:24 +0100

    HW 22_02_2021 Tutorial Tour of Heroes 5.Add in-app navigation with routing

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -9,12 +9,20 @@ import {MessageService} from './message.service';
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService) {
+  }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-  constructor(private messageService: MessageService) {
+  getHero(id: number): Observable<Hero> {
+
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
+
+
 }
