a009e0e0ff558b93398e502b7b3f9bcae8032b5e
Ovsiannikov78 <seri_tanya@mail.ru>
2021-03-08 20:36:39 +0100

    08_03_2021 contacts angular - Doesn't work / Finalize settings

--- contacts-angular/src/app/edit-contact/edit-contact.component.ts
+++ contacts-angular/src/app/edit-contact/edit-contact.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {ContactService} from '../service/contact.service';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {Contact} from '../model/Contact';
 
 @Component({
   selector: 'app-edit-contact',
@@ -7,10 +11,26 @@ import {Component, OnInit} from '@angular/core';
 })
 export class EditContactComponent implements OnInit {
 
-  constructor() {
+  contact: Contact = {};
+
+  constructor(private contactService: ContactService,
+              private route: ActivatedRoute,
+              private location: Location) {
   }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.contactService.getContact(id).subscribe(data => this.contact = data);
+  }
+
+  onSave(): void {
+    this.contactService.editContact(this.contact).subscribe(
+      (_) => this.location.back()
+    );
+  }
+
+  onCancel(): void {
+    this.location.back();
   }
 
 }
